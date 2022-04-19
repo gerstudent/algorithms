d042ab39e0211e882e0cdff8728a4d0533b0ed54
German <gkolcherin@gmail.com>
2022-04-19 13:06:43 +0300

    Updated bubble-sort.cpp

--- Sortings/bubble-sort.cpp
+++ Sortings/bubble-sort.cpp
@@ -2,18 +2,21 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-void bubblesort(int *a, int n){
-    for (int i = 0; i < n - 1; ++i) {
+void bubblesort(int a[], int n){
+    bool swapflag;
+    for (int i = 0; (i < n - 1) && (swapflag) ; ++i) {
+        swapflag = false;
         for (int j = 0; j < n - i - 1; ++j) {
-            if (a[j] > a[j + 1]){
+            if (a[j] > a[j + 1]) {
+                swapflag = true;
                 swap(a[j], a[j + 1]);
             }
         }
     }
 }
 
-void print(int *a, int size){
-    for (int i = 0; i < size; ++i) {
+void print(int *a, int n){
+    for (int i = 0; i < n; ++i) {
         cout << a[i] << " ";
     }
     cout << endl;
