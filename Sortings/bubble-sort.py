1f1378ccc625ebdf57d21a0deb6106b728653085
German <gkolcherin@gmail.com>
2022-04-19 13:09:27 +0300

    Updated bubble-sort.py

--- Sortings/bubble-sort.py
+++ Sortings/bubble-sort.py
@@ -1,12 +1,16 @@
 def bubblesort(a):
     n = len(a)
     for i in range(n):
+        swapflag = False
         for j in range(n - i - 1):
             if a[j] > a[j + 1]:
+                swapflag = True
                 a[j], a[j + 1] = a[j + 1], a[j]
+        if not swapflag:
+            break
+
 
 a = list(map(int, input().split()))
 bubblesort(a)
 for k in range(len(a)):
     print(a[k], end=" ")
-
